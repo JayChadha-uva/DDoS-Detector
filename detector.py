6808a80b743cd3bb50b57da54ce16de54e0a90ae
JayChadha-uva <89432060+JayChadha-uva@users.noreply.github.com>
2022-11-29 14:44:38 -0500

    Improved readilibility

--- detector.py
+++ detector.py
@@ -1,24 +1,18 @@
 import dpkt
-import datetime
 import socket
-from dpkt.compat import compat_ord
 
-# Function from dpkt examples.print_http_requests
 def inet_to_str(inet):
     try:
         return socket.inet_ntop(socket.AF_INET, inet)
     except ValueError:
         return socket.inet_ntop(socket.AF_INET6, inet)
     
-    
 def synFlood(input:str):
     f = open(input, 'rb')
-
     pcap = dpkt.pcap.Reader(f)
     
     ipDst = {}
     ipSrc = {}
-    
     synAckSeen = set()
     ackDict = set()
     
@@ -27,21 +21,16 @@ def synFlood(input:str):
     ackCounter = 0
     
     for num, (ts, buff) in  enumerate(pcap):
-
         try:
             eth = dpkt.ethernet.Ethernet(buff)
         except:
             continue
-
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
             continue
         ip = eth.data
-
         if ip.p != dpkt.ip.IP_PROTO_TCP:
             continue
-
         tcp = ip.data
-
         
         if ((tcp.flags & dpkt.tcp.TH_ACK) and (tcp.flags & dpkt.tcp.TH_SYN)):
             
@@ -60,14 +49,12 @@ def synFlood(input:str):
             if inet_to_str(ip.dst) not in ipDst:
                 ipDst[inet_to_str(ip.dst)] = 1
             else:
-                ipDst[inet_to_str(ip.dst)] = ipDst[inet_to_str(ip.dst)] + 1
-            
+                ipDst[inet_to_str(ip.dst)] = ipDst[inet_to_str(ip.dst)] + 1 
             
         elif ((tcp.flags & dpkt.tcp.TH_ACK) and not (tcp.flags & dpkt.tcp.TH_SYN)):
             if inet_to_str(ip.dst) not in ackDict:
                 ackCounter +=1
-                ackDict.add(inet_to_str(ip.dst))
-                
+                ackDict.add(inet_to_str(ip.dst))  
                 
         if (synCounter - synAckCounter > 100 and synCounter - ackCounter > 100):
             return "Syn flood detected - three way handshake could not be completed. " + str(num) + " loop iterations completed before detected\n"
@@ -77,41 +64,31 @@ def synFlood(input:str):
         
     return "No Syn Flood detected\n"
 
-
 def synAckFlood(input:str):
     f = open(input, 'rb')
-
     pcap = dpkt.pcap.Reader(f)
     
     ipDst = {}
-    
     synAckSeen = {}    
     synCounter = 0
     
     for num, (ts, buff) in  enumerate(pcap):
-
         try:
             eth = dpkt.ethernet.Ethernet(buff)
         except:
             continue
-
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
             continue
         ip = eth.data
-
         if ip.p != dpkt.ip.IP_PROTO_TCP:
             continue
-
         tcp = ip.data
-
         
-        if ((tcp.flags & dpkt.tcp.TH_ACK) and (tcp.flags & dpkt.tcp.TH_SYN)):
-            
+        if ((tcp.flags & dpkt.tcp.TH_ACK) and (tcp.flags & dpkt.tcp.TH_SYN)):        
             if inet_to_str(ip.dst) not in synAckSeen:
                 synAckSeen[inet_to_str(ip.dst)] = 1
-                
-            else:
     
+            else:
                 synAckSeen[inet_to_str(ip.dst)] = synAckSeen[inet_to_str(ip.dst)] + 1
                 
                 temp = 0
@@ -119,8 +96,7 @@ def synAckFlood(input:str):
                     temp = ipDst[inet_to_str(ip.dst)]
                 
                 if  synAckSeen[inet_to_str(ip.dst)] - temp > 60:
-                    return "Syn Ack Flood detected flooding destination " + inet_to_str(ip.dst) + " - " + str(num) + " loop iterations completed before detected\n"
-                
+                    return "Syn Ack Flood detected flooding destination " + inet_to_str(ip.dst) + " - " + str(num) + " loop iterations completed before detected\n"         
                 
         elif (not (tcp.flags & dpkt.tcp.TH_ACK) and (tcp.flags & dpkt.tcp.TH_SYN)):
             synCounter += 1
@@ -129,17 +105,12 @@ def synAckFlood(input:str):
                 ipDst[inet_to_str(ip.dst)] = 1
             else:
                 ipDst[inet_to_str(ip.dst)] = ipDst[inet_to_str(ip.dst)] + 1
-                            
-        
+                              
     return "No SynAck Flood detected\n"    
-
-
-
 
 def nullUDP(input:str):
     f = open(input, 'rb')
     pcap = dpkt.pcap.Reader(f)
-    
     badUpdLength = 0
     goodUpdLength = 0
     
@@ -151,10 +122,8 @@ def nullUDP(input:str):
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
             continue
         ip = eth.data
-
         if ip.p != dpkt.ip.IP_PROTO_UDP:
             continue
-
         udp = ip.data
         
         if udp.ulen == 0:
@@ -167,7 +136,6 @@ def nullUDP(input:str):
    
     return "No null UDP length detected\n"
 
-
 def icmpEcho(input:str):
     f = open(input, 'rb')
     pcap = dpkt.pcap.Reader(f)
@@ -178,7 +146,6 @@ def icmpEcho(input:str):
     for num, (ts, buff) in  enumerate(pcap):
         if num == 0:
             start = ts
-
         try:
             eth = dpkt.ethernet.Ethernet(buff)
         except:
@@ -189,7 +156,6 @@ def icmpEcho(input:str):
         
         if isinstance(ip.data, dpkt.icmp.ICMP):
             icmp = ip.data
-
             if icmp.type == 8:
                 if inet_to_str(ip.dst) not in ipDst:
                     ipDst[inet_to_str(ip.dst)] = 1
@@ -200,7 +166,7 @@ def icmpEcho(input:str):
                 if inet_to_str(ip.dst) in ipDst and ipDst[inet_to_str(ip.dst)] > 60 and delta < 0.5:
                     return "ICMP flood of type 8 (echo). "+ inet_to_str(ip.dst) + " flooded with echo requests. " + str(ipDst[inet_to_str(ip.dst)]) + " requests in " + str(delta) + " seconds.\n"
 
-    return "No ICMP flood detected\n"
+    return "No ICMP echo flood detected\n"
 
 if __name__ == '__main__':
     print(synFlood("SYN.pcap"))
@@ -222,7 +188,5 @@ if __name__ == '__main__':
     # print(nullUDP('pkt.TCP.synflood.spoofed.pcap'))
     
     print(icmpEcho('pkt.ICMP.largeempty.pcap'))
-    
-    
-
+    print(icmpEcho('SYN.pcap'))
     
