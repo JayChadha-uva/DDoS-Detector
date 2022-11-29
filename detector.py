e431537a88319c5beb82de1934505e32e869c5c2
JayChadha-uva <89432060+JayChadha-uva@users.noreply.github.com>
2022-11-29 09:49:19 -0500

    SynAck Flood detection

--- detector.py
+++ detector.py
@@ -9,10 +9,11 @@ def inet_to_str(inet):
         return socket.inet_ntop(socket.AF_INET6, inet)
     
     
-def synFlood():
+def synFlood(input:str):
     # must be in the same directory
-    # f = open('pkt.TCP.synflood.spoofed.pcap', 'rb')
-    f = open('SYN.pcap', 'rb')
+    # f = open(, 'rb')
+    f = open(input, 'rb')
+
     pcap = dpkt.pcap.Reader(f)
     
     ipDst = {}
@@ -69,17 +70,81 @@ def synFlood():
                 
                 
         if (synCounter - synAckCounter > 100 and synCounter - ackCounter > 100):
-            return "Syn flood detected - three way handshake could not be completed. " + str(num) + " loop iterations completed before detected"
+            return "Syn flood detected - three way handshake could not be completed. " + str(num) + " loop iterations completed before detected\n"
         
-        if inet_to_str(ip.src) in ipSrc and ipSrc[inet_to_str(ip.src)] >40:
-            return "Syn Flood detected from IP " + inet_to_str(ip.src) + " - " + str(num) + " loop iterations completed before detected"
+        if inet_to_str(ip.src) in ipSrc and ipSrc[inet_to_str(ip.src)] >60:
+            return "Syn Flood detected from IP " + inet_to_str(ip.src) + " - " + str(num) + " loop iterations completed before detected\n"
         
-    return "No Syn FLood detected"
+    return "No Syn Flood detected\n"
+
+
+def synAckFlood(input:str):
+    # must be in the same directory
+    # f = open('amp.TCP.reflection.SYNACK.pcap', 'rb')
+    f = open(input, 'rb')
+
+    pcap = dpkt.pcap.Reader(f)
     
+    ipDst = {}
+    
+    synAckSeen = {}    
+    synCounter = 0
+    
+    for num, (ts, buff) in  enumerate(pcap):
 
+        try:
+            eth = dpkt.ethernet.Ethernet(buff)
+        except:
+            continue
 
+        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+            continue
+        ip = eth.data
 
+        if ip.p != dpkt.ip.IP_PROTO_TCP:
+            continue
+
+        tcp = ip.data
+
+        
+        if ((tcp.flags & dpkt.tcp.TH_ACK) and (tcp.flags & dpkt.tcp.TH_SYN)):
+            
+            if inet_to_str(ip.dst) not in synAckSeen:
+                synAckSeen[inet_to_str(ip.dst)] = 1
+                
+            else:
+    
+                synAckSeen[inet_to_str(ip.dst)] = synAckSeen[inet_to_str(ip.dst)] + 1
+                
+                temp = 0
+                if inet_to_str(ip.dst) in ipDst:
+                    temp = ipDst[inet_to_str(ip.dst)]
+                
+                if  synAckSeen[inet_to_str(ip.dst)] - temp > 60:
+                    return "Syn Ack Flood detected flooding destination " + inet_to_str(ip.dst) + " - " + str(num) + " loop iterations completed before detected\n"
+                
+                
+        elif (not (tcp.flags & dpkt.tcp.TH_ACK) and (tcp.flags & dpkt.tcp.TH_SYN)):
+            synCounter += 1
+                       
+            if inet_to_str(ip.dst) not in ipDst:
+                ipDst[inet_to_str(ip.dst)] = 1
+            else:
+                ipDst[inet_to_str(ip.dst)] = ipDst[inet_to_str(ip.dst)] + 1
+                            
+        
+    return "No SynAck Flood detected\n"    
 
 
 if __name__ == '__main__':
-    print(synFlood())
+    print(synFlood("SYN.pcap"))
+    print(synFlood('pkt.TCP.synflood.spoofed.pcap'))
+    print(synFlood('part1.pcap'))
+    
+    print(synAckFlood('amp.TCP.reflection.SYNACK.pcap'))
+    print(synAckFlood('SYN.pcap'))
+    print(synAckFlood('pkt.TCP.synflood.spoofed.pcap'))
+    print(synAckFlood('part1.pcap'))
+    
+    
+    
